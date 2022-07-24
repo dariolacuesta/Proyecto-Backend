756dc7f8f2d84e9e4f59379594a088717794baba
dario <dario.lacuesta@hotmail.com>
2022-07-24 11:14:26 -0300

    x

--- Ej25-DenoServer/server.ts
+++ Ej25-DenoServer/server.ts
@@ -1,17 +1,13 @@
 import { Application } from "oak";
-import apiRoutes from "@routers/index.router.ts"
-import logger from "@middlewares/logger.ts"
+import apiRoutes from "@routers/index.router.ts";
+import logger from "@middlewares/logger.ts";
 
-const PORT = Deno.env.get('PORT') || 8080;
+const PORT = Deno.env.get("PORT") || 8080;
 
 const app = new Application();
 
-// app.use((ctx)=>{
-//     ctx.response.type = "text.html";
-//     ctx.response.body = "<h2>Holaaa</h2>"
-// });
-app.use(logger)
+app.use(logger);
 app.use(apiRoutes.routes());
 
 console.log("server running");
-await app.listen({port: +PORT});
+await app.listen({ port: +PORT });
